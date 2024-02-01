b5a7350f2c4cdd6a4c08c0dbbec297052e20247a
Marz <sathickjaffer44@gmail.com>
2024-02-01 14:49:03 +0530

    finished

--- components/languageSelector.js
+++ components/languageSelector.js
@@ -2,48 +2,46 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const LanguageSelector = () => {
-  const { i18n } = useTranslation();
+ const { i18n } = useTranslation();
 
-  const [selectedLanguage, setSelectedLanguage] = useState(null);
+ const [selectedLanguage, setSelectedLanguage] = useState(null);
 
-  const changeToEnglish = () => {
+ const changeToEnglish = () => {
     i18n.changeLanguage("en");
     setSelectedLanguage("english");
-  };
+ };
 
-  const changeToMalayalam = () => {
+ const changeToMalayalam = () => {
     i18n.changeLanguage("ml");
     setSelectedLanguage("malayalam");
-  };
+ };
 
-  useEffect(() =>{
-    if(i18n.language === 'en') 
-      setSelectedLanguage("english")
-    else if(i18n.language === 'ml')
-     setSelectedLanguage("malayalam")
+ useEffect(() => {
+    if (i18n.language === 'en') 
+      setSelectedLanguage("english");
+    else if (i18n.language === 'ml')
+      setSelectedLanguage("malayalam");
     else 
-     setSelectedLanguage('english')
-    
-  },[])
+      setSelectedLanguage('english');
+ }, []);
 
-  return (
+ return (
     <div className="flex flex-row justify-center mb-2 font-playfair">
       <div
         onClick={changeToEnglish}
-        className={`mb-2 mx-6 p-2 cursor-pointer bg-slate-800 rounded-md inline-flex items-center bg-white text-gray-700 shadow-lg rounded-full px-2 py-1 text-sm font-medium cursor-pointer select-none outline-none ring-0 transition-colors duration-280 ease-in-out transform translate-y-0 opacity-100 will-change-transform will-change-opacity z-0 ${selectedLanguage === 'english' ? "bg-[#0C090A] text-white" : ""}`}>
+        className={`mb-2 mx-6 p-2 cursor-pointer rounded-md inline-flex items-center shadow-lg rounded-full px-2 py-1 text-sm font-medium cursor-pointer select-none outline-none ring-0 transition-colors duration-280 ease-in-out transform translate-y-0 opacity-100 will-change-transform will-change-opacity z-0 ${selectedLanguage === 'english' ? "bg-slate-800 text-white" : "bg-white text-gray-700"}`}
+      >
         English
       </div>
 
       <div
         onClick={changeToMalayalam}
-        className={`mb-2 mx-6 p-2 cursor-pointer bg-slate-800 rounded-md inline-flex items-center bg-white text-gray-700 shadow-lg rounded-full px-2 py-1 text-sm font-medium cursor-pointer select-none outline-none ring-0 transition-colors duration-280 ease-in-out transform translate-y-0 opacity-100 will-change-transform will-change-opacity z-0 ${selectedLanguage==='malayalam' ? "bg-[#0C090A] text-white" : ""}`}
+        className={`mb-2 mx-6 p-2 cursor-pointer rounded-md inline-flex items-center shadow-lg rounded-full px-2 py-1 text-sm font-medium cursor-pointer select-none outline-none ring-0 transition-colors duration-280 ease-in-out transform translate-y-0 opacity-100 will-change-transform will-change-opacity z-0 ${selectedLanguage === 'malayalam' ?  "bg-slate-800 text-white" : "bg-white text-gray-700"}`}
       >
         Malayalam
       </div>
     </div>
-  );
+ );
 };
 
 export default LanguageSelector;
-
-
